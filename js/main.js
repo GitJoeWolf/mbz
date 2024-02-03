dff5c13bb62416c6b56b3b6c2f8b55fb8b7d634a
GitJoeWolf <yousofwolf24@gmail.com>
2024-02-03 21:50:14 +0200

    upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function down() {
+    var doc = document.getElementById('drop');
+
+    if (doc.style.display ==  'block')
+    {
+        doc.style.display =  'none';
+    }
+    else
+    {
+        doc.style.display =  'block';   
+    } 
+}
+function up() {
+    var doc = document.getElementById('drop');
+
+    if (doc.style.display ==  'block')
+    {
+        doc.style.display =  'none';
+    }
+}
